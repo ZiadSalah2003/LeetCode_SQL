12f8a92eb1a4a4648b0d780d268825269d67ed27
ZiadSalah2003 <ziadsalah2003@gmail.com>
2024-01-09 15:59:13 +0200

    Update difficulty level of problem

--- 1757. Recyclable and Low Fat Products.sql
+++ 1757. Recyclable and Low Fat Products.sql
@@ -1,7 +1,7 @@
 --Problem Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
 --Problem Name: Recyclable and Low Fat Products
 --Problem ID: 1757
---Difficulty: Medium
+--Difficulty: Easy
 select product_id
 from Products
 where low_fats = 'Y' and recyclable = 'Y'
